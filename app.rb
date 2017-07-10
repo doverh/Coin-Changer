747ca26bceaa5d777a62d0a8d36361ad876980e2
unknown <doverheld@gmail.com>
2017-07-10 13:19:33 -0400

    Add method

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'json'
 require_relative 'coin_changer.rb' 
 enable 'sessions'
 
@@ -16,6 +17,22 @@ end
 	 # teste = res.assoc("quarters")
 	 erb:result, :locals => {:results=>session[:results]}
 
+end
+
+ post '/submit_answer_app' do
+  
+  email = JSON.parse(params[:email])
+  # input_answer =  params[:input_answer].to_i
+  # tokens = params["tokens"].to_i - 1
+  # question_id = params["question_id"]
+  # date = Time.now.strftime("%d/%m/%Y %H:%M:%S")
+  #  db = connection()
+  #   db.exec("INSERT INTO answers (user_name, answer, date, question_id) VALUES ('#{email}', '#{answer}', '#{date}', '#{question_id}')")
+  #   db.exec("UPDATE user_accounts SET tokens='#{tokens}' WHERE email = '#{email}'")
+  # db.close
+  #session[:question_id] = ''
+  #redirect '/start_game'
+  "Post successful - thanks for the info!#{email}"  # feedback for Xcode console (successful POST)
 end
 
 
